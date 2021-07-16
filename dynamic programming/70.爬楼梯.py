3ffe3267df252818e79c48de0621f0151def58ce
myalos <myalos@sjtu.edu.cn>
2021-07-16 16:33:52 +0800

    add .

---
+++ dynamic programming/70.爬楼梯.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode.cn id=70 lang=python3
+#
+# [70] 爬楼梯
+#
+
+# @lc code=start
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1 or n == 2:
+            return n
+        dp = [0] * (n + 1) 
+        dp[1], dp[2] = 1, 2
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
+        
+# @lc code=end
+
